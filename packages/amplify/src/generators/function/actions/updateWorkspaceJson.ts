e22030929cc1c98d46eb15e742724f6eb5e5333d
Michael Gustmann <michael@gustmann.com>
2021-05-11 00:30:05 +0200

    fix: external dependencies should be set to the default
    by default all depencies will be external

--- packages/amplify/src/generators/function/actions/updateWorkspaceJson.ts
+++ packages/amplify/src/generators/function/actions/updateWorkspaceJson.ts
@@ -8,10 +8,6 @@ import { NormalizedSchema } from './normalized-schema';
 export function updateWorkspaceJson(host: Tree, options: NormalizedSchema) {
   const projectConfig = readProjectConfiguration(host, options.projectName);
   projectConfig.targets.build.options.outputPath = `${options.amplifyFunctionPath}/src`;
-  projectConfig.targets.build.options.externalDependencies = [
-    'aws-sdk',
-    'aws-lambda',
-  ];
   projectConfig.targets.build.configurations.production.extractLicenses = false;
   projectConfig.targets.build.configurations.production.sourceMap = false;
   projectConfig.targets.build.configurations.production.buildOptimizer = true;
